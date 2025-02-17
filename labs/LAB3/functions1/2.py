30a2ea5bf8f39e23fb4baca4715adc1616c4510a
Dias <serikdias009@gmail.com>
2025-02-17 18:12:45 +0500

    upd

--- labs/LAB3/functions1/2.py
+++ labs/LAB3/functions1/2.py
@@ -1,9 +1,9 @@
 
-def myfunction(F):
+def myfunCtion(F):
     C = ( 5 / 9) * ( F - 32 )
     return C
 
 
 F = int(input("Enter in Fahrenheit: "))
 
-print("In Celsius:",myfunction(F))
+print("In Celsius:",myfunCtion(F))
